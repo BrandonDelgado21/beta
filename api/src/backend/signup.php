5b2c0b5f91306212a1794bdb722f3f4d8fe0653e
Brandon <bydelgado.2258@unicesmag.edu.co>
2024-10-10 12:03:29 -0500

    Login

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -7,14 +7,29 @@
 
   $email = $_POST['email'];
   $pass = $_POST['passwd'];
+
+  //encrypt password using md5 hashing algorithm
   $enc_pass = md5($pass);
+
+//validate if email already exists
+  $query = "SELECT * FROM users WHERE email = '$email'";
+  $result = pg_query($conn, $query);
+  $row = pg_fetch_assoc($result);
+  if($row){
+    echo "<script>alert('Email already exists')</script>";
+    header('refresh:0; url=//127.0.0.1/beta/api/src/signup_form.html'); //redirect to signup page if email already exists
+    exit();
+  }
+
   //Query to insert data into users table
   $query = "INSERT INTO users (email, password) 
   VALUES ('$email', '$enc_pass')";
+  //Execute the query
   $result = pg_query($conn, $query);
  
   if($result){
-    echo "Registration successful!";
+    echo "<script>alert('Registration succesful')</script>";
+    header('refresh:0; url=//127.0.0.1/beta/api/src/login_form.html'); //redirect to login page if registration successful
   }else{
     echo "Registration failed! ";
   }
